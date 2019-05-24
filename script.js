a32998c44e17cbc6451b1c2bf8ec65c6d5b4f26d
Ismail Vittal <ismailvtl@gmail.com>
2019-05-24 09:39:18 +0400

    Update script.js

--- script.js
+++ script.js
@@ -131,7 +131,7 @@ function closeAddView() {
 
 function diff_months(dt2, dt1) 
  {
-  var diff =(dt2.getTime() - dt1.getTime()) / 2000;
+  var diff =(dt2.getTime() - dt1.getTime()) / 1000;
    diff /= (60 * 60 * 24 * 7 * 4);
   return Math.abs(Math.round(diff));
   
